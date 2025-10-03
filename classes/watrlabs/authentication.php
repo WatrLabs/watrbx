45f61da7ffeb0c43dad55b5ee511143098c12b0f
danyalassi <70813522+danyalassi@users.noreply.github.com>
2025-10-03 23:10:08 +0200

    Fixed missing use of fastflag

--- classes/watrlabs/authentication.php
+++ classes/watrlabs/authentication.php
@@ -6,6 +6,7 @@ use watrbx\sitefunctions;
 use watrlabs\users\getuserinfo;
 use watrlabs\watrkit\sanitize;
 use watrlabs\logging\discord;
+use watrlabs\fastflags;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use Pixie\Connection;
