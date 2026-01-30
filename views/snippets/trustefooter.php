99242d36e513e600db10e0fb9699611a0a67dd26
Gjhkyio <147998329+Gjhkyio@users.noreply.github.com>
2026-01-30 14:05:38 -0300

    Update trademark information in trustefooter.php

--- views/snippets/trustefooter.php
+++ views/snippets/trustefooter.php
@@ -29,8 +29,8 @@
             </div>
             <div class="right">
                 <p class="Legalese">
-    ROBLOX, "Online Building Toy", characters, logos, names, and all related indicia are trademarks of <a href="https://corp.watrbx.wtf/" ref="footer-smallabout" class="roblox-interstitial">ROBLOX Corporation</a>, ©2015. Patents pending.
-    ROBLOX is not sponsored, authorized or endorsed by any producer of plastic building bricks, including The LEGO Group, MEGA Brands, and K'Nex, and no resemblance to the products of these companies is intended.
+    WATRBX, "Online Building Toy", characters, logos, names, and all related indicia are trademarks of <a href="https://corp.watrbx.wtf/" ref="footer-smallabout" class="roblox-interstitial">WATRBX</a>, ©<?= date('Y'); ?>. Patents pending.
+    WATRBX is not sponsored, authorized or endorsed by any producer of plastic building bricks, including The LEGO Group, MEGA Brands, and K'Nex, and no resemblance to the products of these companies is intended.
     Use of this site signifies your acceptance of the <a href="https://www.watrbx.wtf/info/terms-of-service" ref="footer-terms">Terms and Conditions</a>.
 </p>
             </div>
